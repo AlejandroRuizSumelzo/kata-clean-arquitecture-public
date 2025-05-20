304b1855e0097c6123dc60d45ce81503f65c25e2
Alejandro Ruiz <alejandroruizsumelzo@gmail.com>
2025-05-20 11:01:05 +0200

    Upload some email test

--- domain/tests/email.test.ts
+++ domain/tests/email.test.ts
@@ -6,4 +6,22 @@ describe("Email", () => {
       Email.create("");
     }).toThrow("Email cannot be empty");
   });
+
+  it("should convert email to lowercase", () => {
+    expect(() => {
+      const email = Email.create("XURXODEV@TEST.COM");
+      expect(email.value).toBe("xurxodev@test.com");
+    });
+  });
+
+  it("should throw error for invalid email format", () => {
+    expect(() => {
+      Email.create("invalid-format-email");
+    }).toThrow("Invalid email format");
+  });
+
+  it("should create a valid email", () => {
+    const email = Email.create("test@test.com");
+    expect(email.value).toBe("test@test.com");
+  });
 });
