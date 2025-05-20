ca4c2d7348672898450f5b908ca040bf45e5d29b
Alejandro Ruiz <alejandroruizsumelzo@gmail.com>
2025-05-20 19:02:43 +0200

    save user test

---
+++ domain/use-cases/user/tests/save-user.test.ts
@@ -0,0 +1,34 @@
+import { UserMock } from "../../../../data/mocks/user-mock";
+import { User } from "../../../entities/user";
+import { UserRepository } from "../../../repository/user-repository";
+import { SaveUserUseCase } from "../save-user-use-case";
+
+const mockUserRepository: UserRepository = {
+  getUsers: jest.fn(),
+  saveUser: jest.fn(),
+  getUserByEmail: jest.fn(),
+};
+
+describe("SaveUserUseCase", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should save successfully a new valid user", async () => {
+    const testUser = User.create(
+      "John Doe",
+      "john@example.com",
+      "Password123!"
+    );
+    (mockUserRepository.getUserByEmail as jest.Mock).mockResolvedValue(null);
+    (mockUserRepository.saveUser as jest.Mock).mockResolvedValue(undefined);
+
+    const useCase = new SaveUserUseCase(mockUserRepository);
+    await useCase.execute(testUser);
+
+    expect(mockUserRepository.getUserByEmail).toHaveBeenCalledWith(
+      testUser.email
+    );
+    expect(mockUserRepository.saveUser).toHaveBeenCalledWith(testUser);
+  });
+});
